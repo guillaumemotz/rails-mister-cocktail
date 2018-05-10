64e7fb5976a4e4235ba5cf294aff182c2831bd90
guillaume motz <guillaume.motz@gmail.com>
2018-05-10 14:44:22 +0200

    all routes ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,4 +7,7 @@ class CocktailsController < ApplicationController
 
   def new
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-
-  get 'doses/create'
-
-  get 'doses/delete'
-
-  get 'cocktails/index'
-
-  get 'cocktails/show'
-
-  get 'cocktails/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: [ :destroy ]
 end
+
+
+# CocktailsController
+#   routing
+#     routes to #index (FAILED - 16)
+#     routes to #new
+#     routes to #show (FAILED - 17)
+#     routes to #create (FAILED - 18)
+
+# DosesController
+#   routing
+#     routes to #new (FAILED - 19)
+#     routes to #create (FAILED - 20)
+#     routes to #destroy (FAILED - 21)
